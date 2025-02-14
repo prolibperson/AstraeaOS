fd91424161902c16ea23b43ec05ceb5f4e558cf9
prolib <prolib69420@gmail.com>
2025-02-13 23:20:46 -0500

    oops

--- src/limine/limine_manager.c
+++ src/limine/limine_manager.c
@@ -1,7 +1,6 @@
 #include "limine_manager.h"
 #include "cpu_manager.h"
 #include "limine.h"
-//#include "serial.h"  // Assuming you have a serial port driver for debugging
 
 __attribute__((used, section(".limine_requests")))
 static volatile LIMINE_BASE_REVISION(3);
@@ -15,21 +14,16 @@ static volatile struct limine_framebuffer_request framebuffer_request = {
 /* init limine */
 void limine_init(void) {
     if (!LIMINE_BASE_REVISION_SUPPORTED) {
-       // serial_printf("Limine base revision not supported\n");
         halt();
     }
 
     if (framebuffer_request.response == NULL) {
-        //serial_printf("Framebuffer request response is NULL\n");
         halt();
     }
 
     if (framebuffer_request.response->framebuffer_count < 1) {
-       // serial_printf("No framebuffers available\n");
         halt();
     }
-
-   // serial_printf("Limine initialized successfully\n");
 }
 
 /* fetch framebuffer */
